2fd94de2169f5d7650857604449b37f841c31f97
Heather Kraft <heather.kraft@colorado.edu>
2020-09-09 20:34:47 -0600

    working on starting to build out something. Going to build a little heather website. Will most certainly be a work in progress, but perhaps I can progress towards a goal. Tried to connect the button to the image, but was running into issue. Need to teach myself some more js

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 // holder place for javascript
-let btnList = document.getElementsByTagName('button');
-let btn = btnList[0];
+ let btnList = document.getElementsByTagName('button');
+ let btn = btnList[0];
 
-let boxList = document.getElementsByClassName('box');
-let box = boxList[0];
+//let boxList = document.getElementsByClassName('box');
+//let box = boxList[0];
 
-let isBoxShowing = false;
+let imgList = document.getElementsByClassName('img');
+let img = imgList[0];
 
-btn.addEventListener('click', () => {
-    console.log('isBoxShowing', isBoxShowing)
-    if (isBoxShowing) {
-        box.style.display = 'none';
-        isBoxShowing = false;
+let isImgShowing = false;
+
+// let isBoxShowing = false;
+
+// btn.addEventListener('click', () => {
+ //   console.log('isBoxShowing', isBoxShowing)
+   // if (isBoxShowing) {
+     //   box.style.display = 'none';
+       // isBoxShowing = false;
+    // } else {
+       // box.style.display = 'block';
+        // isBoxShowing = true;
+   // }
+// });
+
+
+
+img.addEventListener('click', () => {
+    console.log('isImgShowing', isImgShowing)
+    if (isImgShowing) {
+        img.style.display = 'none';
+        isImgShowing = false;
     } else {
-        box.style.display = 'block';
-        isBoxShowing = true;
+        img.style.display = 'img';
+        isImgShowing = true;
     }
 });
