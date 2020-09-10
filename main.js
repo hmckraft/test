08882a80e984df0be6f326531a1b9b1c4561f2fd
Heather Kraft <heather.kraft@colorado.edu>
2020-09-10 17:11:29 -0600

    added event listener in js to show/hide image

--- main.js
+++ main.js
@@ -2,36 +2,23 @@
  let btnList = document.getElementsByTagName('button');
  let btn = btnList[0];
 
-//let boxList = document.getElementsByClassName('box');
-//let box = boxList[0];
-
 let imgList = document.getElementsByClassName('img');
 let img = imgList[0];
 
-let isImgShowing = false;
-
-// let isBoxShowing = false;
-
-// btn.addEventListener('click', () => {
- //   console.log('isBoxShowing', isBoxShowing)
-   // if (isBoxShowing) {
-     //   box.style.display = 'none';
-       // isBoxShowing = false;
-    // } else {
-       // box.style.display = 'block';
-        // isBoxShowing = true;
-   // }
-// });
 
 
+ let isimgShowing = false;
 
-img.addEventListener('click', () => {
-    console.log('isImgShowing', isImgShowing)
-    if (isImgShowing) {
+ btn.addEventListener('click', () => {
+    console.log('isimgShowing', isimgShowing)
+    if (isimgShowing) {
         img.style.display = 'none';
-        isImgShowing = false;
-    } else {
-        img.style.display = 'img';
-        isImgShowing = true;
+        isimgShowing = false;
+     } else {
+        img.style.display = 'block';
+         isimgShowing = true;
     }
-});
+ });
+
+
+
